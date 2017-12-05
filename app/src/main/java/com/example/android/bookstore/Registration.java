18fb7722a2ddd4298650c36421a685d9e018a2a4
hgadirli <hblgdrl8@gmail.com>
2017-12-06 03:08:14 +0400

    Test Commit

---
+++ app/src/main/java/com/example/android/bookstore/Registration.java
@@ -0,0 +1,16 @@
+package com.example.android.bookstore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Registration extends AppCompatActivity {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
